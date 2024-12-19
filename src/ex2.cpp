9ef62c3b203a93f0c73243e7c9d6e4f89a6c935e
Dan Piponi <dpiponi@gmail.com>
2024-12-19 11:03:08 -0800

    minor

--- src/ex2.cpp
+++ src/ex2.cpp
@@ -6,6 +6,15 @@
 
 #include "ChanceScript.h"
 
+template<typename T>
+void Dump(const Dist<T>& dist)
+{
+  for (auto z : dist.pdf)
+  {
+      std::cout << z.value << ' ' << z.prob << std::endl;
+  }
+}
+
 // [1,3,5] < [1,2,3,4]
 // [1,3,5,7] !< [1,3]
 template<typename T>
@@ -15,14 +24,28 @@ bool subdist(const Dist<T>& a, const Dist<T>& b)
   int j = 0;
   while (i < a.pdf.size() && j < b.pdf.size())
   {
-    if (a.pdf[i].value < b.pdf[i].value)
+    if (a.pdf[i].value == b.pdf[j].value)
+    {
+      ++i;
+    } else if (a.pdf[i].value < b.pdf[j].value)
     {
       return false;
+    } else
+    if (a.pdf[i].value > b.pdf[j].value)
+    {
+      ++j;
     }
-    ++i;
-    ++j;
   }
-  return i < a.pdf.size();
+  return i == a.pdf.size();
+}
+
+template<typename X, typename F>
+Dist<X> do_as_matrix(const Dist<X>& d, const F& f)
+{
+  for (auto& [v, p] : d.pdf)
+  {
+    auto row = v >> f;
+  }
 }
 
 template<typename X, typename F>
@@ -36,7 +59,12 @@ Dist<X> iterate_i(const X& init, const F& f, int n)
       std::cout << old_r.pdf.size() << '/' << r.pdf.size() << std::endl;
       if (subdist(r, old_r))
       {
-        std::cout << "Explored" << std::endl;
+        std::cout << "Explored old" << std::endl;
+        Dump(old_r);
+        std::cout << "And new" << std::endl;
+        Dump(r);
+        std::cout << "---" << std::endl;
+        do_as_matrix(r, F);
       }
     }
     return r;
@@ -44,9 +72,6 @@ Dist<X> iterate_i(const X& init, const F& f, int n)
 
 int main()
 {
-  auto r = iterate_i(0, [](int x) { return roll(6) >> [=](int y) { return certainly(std::min(1, x + y)); }; }, 3);
-  for (auto z : r.pdf)
-  {
-      std::cout << z.value << ' ' << z.prob << std::endl;
-  }
+  auto r = iterate_i(0, [](int x) { return roll(6) >> [=](int y) { return certainly(std::min(9, x + y)); }; }, 3);
+  Dump(r);
 }
